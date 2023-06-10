6655f321c2ce6c8311463a4b039c668ee042bb64
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-06-10 12:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if (shortHex.startsWith('#')) {
+    shortHex = shortHex.slice(1);
+  }
+
+  // Extend the short hex code to full length
+  let fullHex = '';
+  for (let i = 0; i < shortHex.length; i++) {
+    const char = shortHex[i];
+    // Repeat the character twice to get the full component
+    fullHex += char + char;
+  }
+
+  // Add the '#' prefix to the full hex code
+  return '#' + fullHex;
 };
 
 // Do not change the code below.
